5f50e5675f1aba983696195722c25855cb0b09a8
tgaybare <tgaybare@itba.edu.ar>
2021-07-06 15:31:24 -0300

    Pensamiento e inicio de desarrollo de las funciones del ADT

--- imdbTAD.c
+++ imdbTAD.c
@@ -4,6 +4,7 @@
 
 #include "imdbTAD.h"
 #include <stdlib.h>
+#include <string.h>
 
 #define NO_FIELD 0 // Si el campo no aplica a ese tipo de entrada
 #define PELI 1
@@ -15,6 +16,8 @@ typedef struct TGenre {
     struct TGenre * tail;
 } TGenre;
 
+typedef TGenre * TGenreL;
+
 typedef struct TEntry {
     char * name;  // Nombre de la serie o pelicula
     char type;    // Serie = SERIE y Peli = PELI
@@ -24,18 +27,184 @@ typedef struct TEntry {
     float avgRating;
     unsigned runtimeMin; // Runtime si es peli o NO_FIELD si es serie
     char ** genre;
+    unsigned cantGenres;
 } TEntry;
 
 typedef struct TYear {
+    unsigned year;
     size_t cantPelis;
     size_t cantSeries;
     TEntry peli;  // Peli mas votada
     TEntry serie; // Serie mas votada
-    TGenre * firstGenre;  // Lista de generos de peliculas
+    TGenreL firstGenre;  // Lista de generos de peliculas
+    TGenreL currentGenre;
     struct TYear * tail;  // Puntero al siguiente año
 } TYear;
 
+typedef TYear * TYearL;
+
 typedef struct imdbCDT {
     TYear * firstY;
+    TYear * currentY;
 } imdbCDT;
+
+imdbADT newDataBase(){
+    return calloc(1, sizeof(imdbCDT));
+}
+
+static void freeADTGenre(TGenreL list){
+    if (list == NULL){
+        return;
+    }
+    freeADTGenre(list->tail);
+    free(list->genre);
+    free(list);
+}
+
+static void freeADTYear(TYearL list){
+    if (list == NULL){
+        return;
+    }
+    freeADTYear(list->tail);
+    freeADTGenre(list->firstGenre);
+    free(list->peli.name);
+    free(list->serie.name);
+    for (int i = 0; i < list->serie.cantGenres; ++i) {
+        free(list->serie.genre[i]);
+    }
+    free(list->serie.genre);
+    for (int i = 0; i < list->peli.cantGenres; ++i) {
+        free(list->peli.genre[i]);
+    }
+    free(list->peli.genre);
+    free(list);
+}
+
+void freeADT(imdbADT data){
+    freeADTYear(data->firstY);
+    free(data);
+}
+
+static size_t getMostVotesREC(const TYearL list, unsigned year, char type){
+    if (list == NULL || list->year < year){
+        return 0;
+    }
+    else if (type == PELI && list->year == year){ //el ano es el indicado y es una pelicula
+        return list->peli.numVotes;
+    }
+    else if (list->year == year){ //el ano es el indicado y es una serie
+        return list->serie.numVotes;
+    }
+    else{
+        return getMostVotesREC(list->tail, year, type);
+    }
+}
+
+size_t getMostVotes(imdbADT data, unsigned year, char type){
+    return getMostVotesREC(data->firstY, year, type);
+}
+
+TEntry getMostPopular(const imdbADT data, char type){
+    if (type == PELI){
+        return data->currentY->peli;
+    }
+    else{
+        return data->currentY->serie;
+    }
+}
+
+/* size_t getAmountPerGenre(const imdbADT data){
+    return data->currentY->currentGenre->cant;
+}  NO SERIA LO MISMO QUE ITERAR????  */
+
+/*TGenreL updateCantRECGenres(TGenreL list, char * genre) {
+    if (list == NULL || strcmp(list->genre, genre) > 0){
+        TGenreL new = malloc(sizeof(TGenre));
+        new->genre = genre;
+        new->cant = 1;
+        new->tail = list;
+        return new;
+    }
+    else if (strcmp(list->genre, genre) == 0) {
+        list->cant++;
+        return list;
+    }
+    else {
+        list->tail = updateCantRECGenres(list->tail, genre);
+        return list;
+    }
+}
+
+
+void updateCantREC(TYearL list, unsigned year, char type, char * genre){
+    if (list == NULL || list->year < year){
+        addMov;
+        return;
+    }
+    else if (type == PELI && list->year == year){ //el ano es el indicado y es una pelicula
+        list->cantPelis++;
+        return;
+    }
+    else if (type == SERIE && list->year == year){ //el ano es el indicado y es una serie
+        list->cantSeries++;
+        return;
+    }
+    else if (type == NO_FIELD && list->year == year){ //updatea la cant de generos
+        updateCantRECGenres(list->firstGenre);
+        return;
+    }
+    else{
+        updateCantREC(list->tail, year, type);
+    }
+}
+
+void updateNodes(imdbADT data, unsigned year, char type, char * genre){
+    updateCantREC(data->firstY, year, type,genre);
+}
+
+void update()*/
+
+void toBeginYear(imdbADT data){
+    data->currentY = data->firstY;
+}
+
+int hasNextYear(imdbADT data){
+    if (data->currentY == NULL){
+        return 0;
+    }
+    return 1;
+}
+
+TYearL nextYear(imdbADT data){
+    if (hasNextYear(data) == 0){
+        // ????
+    }
+    else{
+        TYearL aux = data->currentY;
+        data->currentY = data->currentY->tail;
+        return aux;
+    }
+}
+
+void toBeginGenre(imdbADT data){
+    data->currentY->currentGenre = data->currentY->firstGenre;
+}
+
+int hasNextGenre(imdbADT data){
+    if (data->currentY->currentGenre == NULL){
+        return 0;
+    }
+    return 1;
+}
+
+TGenreL nextGenre(imdbADT data){
+    if (hasNextGenre(data) == 0){
+        // ????
+    }
+    else{
+        TGenreL aux = data->currentY->currentGenre;
+        data->currentY->currentGenre = data->currentY->currentGenre->tail;
+        return aux;
+    }
+}
 
--- imdbTAD.h
+++ imdbTAD.h
@@ -5,5 +5,57 @@
 #ifndef TPE_IMDBTAD_H
 #define TPE_IMDBTAD_H
 
+#include <stdio.h>
+
+enum TQuery {Q1 = 1, Q2, Q3};
+
+typedef imdbCDT * imdbADT;
+
+imdbADT newDataBase();
+
+void freeADT(imdbADT data);
+
+/* Devuelve la cantidad de series y de pelis en un ano determinado que esta en el iterador.
+ * VECTOR DE 3 posiciones (ano/cantPelis/cantSeries) */
+unsigned int * getAmount(const imdbADT data);
+
+/* Devuelve la pelicula o serie mas votada de un ano determinado que este en el iterador*/
+TEntry getMostPopular(const imdbADT data, char type);
+
+
+/* Devuelve la cantidad de peliculas y series de un genero especifico que esta en el iterador*/
+size_t getAmountPerGenre(const imdbADT data);
+
+/* devuelve los votos de la pelicula o serie con mas votos de un ano especifico*/
+size_t getMostVotes(imdbADT data, unsigned year, char type);
+
+// updateInicial para agregar peli y serie y su genero si ya existe
+
+/* actualiza la cantidad de pelis/series que hay en un ano o la cantidad de peliculas o series con cierto genero en un ano)*/
+//void updateCant(imdbADT data, unsigned year, char type, char * genre);
+
+//void updateCantGenres(imdbADT data, char * genre);
+
+//void updateMostVoted(imdbADT data);
+
+
+
+/* funciones para iterar */
+
+void toBeginYear(imdbADT data);
+
+int hasNextYear(imdbADT data);
+
+TYearL nextYear(imdbADT data);
+
+
+
+/* funciones para iterar el genero */
+
+void toBeginGenre(imdbADT data);
+
+int hasNextGenre(imdbADT data);
+
+TGenreL nextGenre(imdbADT data);
 
 #endif //TPE_IMDBTAD_H
