d76e91b86a741dac0feffb7de0218944143f4f49
nmargenat <nmargenat@itba.edu.ar>
2021-07-07 17:50:48 -0300

    imdbTAD.h emprolijado + agregado backend.h

--- imdbTAD.h
+++ imdbTAD.h
@@ -1,4 +1,3 @@
-
 #ifndef TPE_IMDBTAD_H
 #define TPE_IMDBTAD_H
 
@@ -9,31 +8,55 @@ enum TQuery {Q1 = 1, Q2, Q3};
 
 typedef struct imdbCDT * imdbADT;
 
+/* Crea una nueva Base de Datos de peliculas y series */
 imdbADT newDataBase();
 
+/* Libera el espacio utilizado por el TAD */
 void freeADT(imdbADT data);
 
 //FRONT
-
 void updateData(imdbADT data, TEntry * entry);
 
 //BACK
-
+/* Resetea el iterador que recorre los años de manera descendente */
 void toBeginYear(imdbADT data);
-int nextYear(imdbADT data); //se fija si hay proximo anio llamando a hasNextYear y si hay aumenta el iterador
+/* Se fija si hay año siguiente */
 int hasNextYear(imdbADT data);
+/*
+ * Se fija si hay año proximo.
+ * - Si hay año proximo mueve el iterador y retorna 1.
+ * - Si no hay año proximo retorna 0.
+ */
+int nextYear(imdbADT data);
 
+/* Resetea el iterador que recorre los generos de manera ascendente, de un determinado año */
 void toBeginG(imdbADT data);
+/* Se fija si hay un genero siguiente */
 int hasNextG(imdbADT data);
-int nextG(imdbADT data); //se fija si hay proximo genero llamando a hasNextG y si hay aumenta el iterador
+/*
+ * Se fija si hay un genero siguiente.
+ * - Si lo hay, mueve el iterador y retorna 1.
+ * - Si no lo hay retorna 0.
+ */
+int nextG(imdbADT data);
 
-
-// Devuelve la cantidad de peliculas y series del anio que esta en el iterador en parametros de salida
+/*
+ * Devuelve en 3 paramentros de salida:
+ * - cantPelis : la cantidad de peliculas del año year.
+ * - cantSeries : la cantidad de series del año year.
+ * - year : año del que estamos sacando la informacion
+ */
 void getAmountCurrY(imdbADT data, size_t * cantPelis, size_t * cantSeries, unsigned * year);
 
-// Devuelve los generos y sus cantidades de peliculas del anio que esta en el iterador
+/*
+ * Devuelve en 3 paramentros de salida:
+ * - year : año del que estamos sacando la informacion
+ * - genero : genero que estamos consultando
+ * - cantPelis : la cantidad de peliculas del año year del genero.
+ */
 void getAmountG(imdbADT data, unsigned * year, const char * genero, size_t * cantPelis);
 
-// Devuelve una copia de la peli/serie con mas votos
+/* Devuelve una copia de la peli/serie con mas votos */
 TEntry * getMostPopular(imdbADT data, char type);
 
+#endif
