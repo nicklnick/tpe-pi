031b9e6b75a7ae01a628e127f6813b57c38f1f23
Marcos <mgronda@itba.edu.ar>
2021-07-08 10:33:04 -0300

    Front end ahora verficia todos los campos, excluyendo los generos

--- frontEnd.c
+++ frontEnd.c
@@ -7,20 +7,25 @@
 #include "dataTypes.h"
 
 #define LINE_MAX 400
-#define BLOCK 10
 
 #define SEPARADOR ";"
 #define SEPARADOR_2 ','
 
 #define OK 0
 #define ERROR_DE_FILE 5
+#define INVALID_YEAR 3
 
-#define PELI 1
-#define SERIE 2
-#define NO_FIELD 0
 #define EMPTY_FIELD "\\N"
 
 #define UPDATE_TOKEN token = strtok(NULL, SEPARADOR);
+
+#define CHECK_ENTRY(token, field, type){ if(!strcmp(token, EMPTY_FIELD))\
+                                         entry->field = NO_FIELD;       \
+                                       else                             \
+                                         entry->field = type(token);    \
+};
+
+
 
 static char * copyText(const char * text, char c){          // Copia hasta \0 o hasta el caracter c
     int i;
@@ -61,7 +66,7 @@ static char ** loadGenres(char * line, unsigned * cant){
 }
 
 
-static void updateEntry(TEntry * entry, char * line){
+static int updateEntry(TEntry * entry, char * line){
     char * token = strtok(line, SEPARADOR);
 
     if(!strcmp(token, "movie"))      // Tipo de entry
@@ -73,13 +78,13 @@ static void updateEntry(TEntry * entry, char * line){
     entry->name = copyText(token, 0);        // Nombre de entry
 
     UPDATE_TOKEN
-    entry->startYear = atoi(token);        // Anio de comienzo
+    if(!strcmp(token, EMPTY_FIELD))
+        return INVALID_YEAR;                   // Verifica si el anio es valido, si no lo eso => corta
+    else
+        entry->startYear = atoi(token);        // Anio de comienzo
 
     UPDATE_TOKEN
-    if(!strcmp(token, EMPTY_FIELD))      // Verifica que el valor no sea \N
-        entry->endYear = NO_FIELD;
-    else
-        entry->endYear = atoi(token);
+    CHECK_ENTRY(token, endYear, atoi);
 
     UPDATE_TOKEN
     unsigned cant;
@@ -87,16 +92,15 @@ static void updateEntry(TEntry * entry, char * line){
     entry->cantGenres = cant;
 
     UPDATE_TOKEN
-    entry->avgRating = atof(token);         // Carga el avgRating
+    CHECK_ENTRY(token, avgRating, atof);
 
     UPDATE_TOKEN
-    entry->numVotes = atoi(token);         // Carga el numVotes
+    CHECK_ENTRY(token, numVotes, atoi);
 
     UPDATE_TOKEN
-    if(!strcmp(token, EMPTY_FIELD))      // Verifica que el valor no sea \N
-        entry->runtimeMin = NO_FIELD;
-    else
-        entry->runtimeMin = atoi(token);
+    CHECK_ENTRY(token, runtimeMin, atoi);
+
+    return OK;
 }
 
 static void freeResources(TEntry * entry){
@@ -124,17 +128,17 @@ int readFile(char fileName[], imdbADT data){
 
     fgets(line, sizeof(line), imdbFile);    // Ignora la primera linea
 
-    int counter= 0;
     while(fgets(line, sizeof(line), imdbFile)) {
-        if(counter==200000)
-            puts("hola");
-        updateEntry(entry, line);
-        updateData(data, entry);
-        counter++;
-        freeResources(entry);
+        if(updateEntry(entry, line)!=INVALID_YEAR){
+            updateData(data, entry);                    // Solo si el entry es valido se actualiza el ADT
+            freeResources(entry);
+        }
+        else{
+            free(entry->name);
+        }
     }
 
-
+    free(entry);
     fclose(imdbFile);       // Cierre final del file
     return OK;
 }
