1be12a6114b6525c51ffef8fed8a85fd244ef8df
nmargenat <nmargenat@itba.edu.ar>
2021-07-09 15:47:43 -0300

    Clarificacion de los headers de errores y dataTypes

--- dataTypes.h
+++ dataTypes.h
@@ -1,54 +1,62 @@
-
 #ifndef TPE_FINAL_DATATYPES_H
 #define TPE_FINAL_DATATYPES_H
 
 #include <stdlib.h>
 #include "errorCodes.h"
 
+/* Tipo de dato que tiene todos los datos de una serie o pelicula */
 typedef struct TEntry {
     char * name;  // Nombre de la serie o pelicula
     char type;    // Serie = SERIE y Peli = PELI
     unsigned numVotes;
     unsigned startYear; // Año de lanzamiento (peli) o comienzo de emision (serie)
-    unsigned endYear;   // Año de fin de emision, o NO_FIELD si no termino o NO_FILED si es peli
+    unsigned endYear;   // Año de fin de emision, o NO_FIELD si sigue en emision o si es peli
     float avgRating;
     unsigned runtimeMin; // Runtime si es peli o NO_FIELD si es serie
     char ** genre;
     unsigned cantGenres;
 } TEntry;
 
+/* Estructura utilizada para devolver lo correspondiente al query1 */
 typedef struct TQuery1 {
     unsigned year;
     unsigned cantPelis;
     unsigned cantSeries;
 } TQuery1;
 
+/* Estructura utilizada para devolver lo correspondiente al query2 */
 typedef struct TQuery2 {
     unsigned year;
     unsigned cantPelis;
     char * genero;
 } TQuery2;
 
-// OKE
+/* Estructura utilizada para devolver lo correspondiente al query3 */
 typedef struct TQuery3 {
     TEntry * peli;
     TEntry * serie;
 } TQuery3;
 
+/* Identificadores */
 #define PELI 1
 #define SERIE 2
 #define NO_FIELD 0
+/**/
 
 #define EMPTY 0             // Si en copyText no se necesita la funcionalidad de SEPARADOR
 #define BLOCK 10
 
-static char * copyText(const char * text, char c, int * error){          // Copia hasta \0 o hasta el caracter c
+/* Copia hasta '\0' o hasta el caracter c */
+static char * copyText(const char * text, char c, int * error)
+{
     int i;
     char * new = NULL, * aux = NULL;
 
-    for(i=0; text[i]!=0 && text[i]!=c; i++){
-        if(i%BLOCK==0){
-            new = realloc(aux, (i+BLOCK)*sizeof(char));
+    for( i = 0 ; text[i] != 0 && text[i] != c; i++ )
+    {
+        if( i % BLOCK == 0 )
+        {
+            new = realloc(aux, (i + BLOCK) * sizeof(char));
             NO_MEM(*error)
             RETURN_IF_ERROR(*error, aux)
             aux = new;
@@ -56,10 +64,9 @@ static char * copyText(const char * text, char c, int * error){          // Copi
         new[i] = text[i];
     }
     // Este realloc nunca deberia tirar error porque "corta" lo que sobra
-    new = realloc(new, (i+1)*sizeof(char));
+    new = realloc(new, (i + 1) * sizeof(char));
     new[i]=0;
     return new;
 }
-
 
 #endif //TPE_FINAL_DATATYPES_H
--- errorCodes.h
+++ errorCodes.h
@@ -1,44 +1,51 @@
-//
-// Created by nicol on 9/7/2021.
-//
-
 #ifndef TPE_IMDB_ERRORCODES_H
 #define TPE_IMDB_ERRORCODES_H
 
 #include <errno.h>
 
-#define OK 0
+/* Mensajes de error */
+#define MSG_MEM "Cantidad de memoria insuficiente"
+#define MSG_FILE "File no existente"
+
+/* Codigos de error */
 #define ERROR_DE_ARGS 20
 #define INSUFFICIENT_MEM 10
 #define ERROR_DE_FILE 5
 #define INVALID_YEAR 3
-#define MSG_MEM "Cantidad de memoria insuficiente"
-#define MSG_FILE "File no existente"
 
+#define OK 0
+
+/* CONJUNTO DE MACROS QUE SE ENCARGAN DEL MANEJO DE ERRORES */
 /* No hay suficiente espacio en memoria */
-#define NO_MEM(error)  {  if( errno == ENOMEM )      \
-                             (error) = INSUFFICIENT_MEM;           \
+#define NO_MEM(error)  {                                    \
+                            if( errno == ENOMEM )           \
+                                (error) = INSUFFICIENT_MEM; \
                        };
 
-/* Hubo error al manipular el file */
-#define FILE_ERROR(file, retValue)  {  if( (file) == NULL )      \
-                                            return retValue;       \
+/* Hubo un error al intentar manipular el file */
+#define FILE_ERROR(file, retValue)  {                            \
+                                        if( (file) == NULL )     \
+                                            return retValue;     \
                                     };
 
 /* Sale de la funcion si hubieron errores */
-#define RETURN_IF_ERROR(error, retValue) { if( (error) != OK )          \
-                                            return retValue; \
+#define RETURN_IF_ERROR(error, retValue) {                          \
+                                            if( (error) != OK )     \
+                                              return retValue;      \
                                          };
 
-#define FREE_ADT(error, data) { if( (error) != OK )      \
-                                  freeADT(data); \
-                              };
+/* Si hubo un error imprime a salida de error y aborta el programa */
+#define ABORT_IF_ERROR(error, errorMsg) {                                   \
+                                            if( (error) != OK ) {           \
+                                                fprintf(stderr, errorMsg);  \
+                                                exit(error);                \
+                                            }                               \
+                                        };
 
-#define ABORT_IF_ERROR(error, errorMsg) { if( (error) != OK ) {     \
-                                  fprintf(stderr, errorMsg);       \
-                                  exit(error);                     \
-                                  }                                 \
+/* Libera el espacio del TAD en caso de error */
+#define FREE_ADT(error, data) {                         \
+                                if( (error) != OK )     \
+                                    freeADT(data);      \
                               };
-
 
 #endif //TPE_IMDB_ERRORCODES_H
