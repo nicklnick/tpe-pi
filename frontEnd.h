8cf88001d01f4ee57bb8540d4e414cd9e3e753e1
nmargenat <nmargenat@itba.edu.ar>
2021-07-08 22:44:53 -0300

    Actualizado frontEnd.h

--- frontEnd.h
+++ frontEnd.h
@@ -5,8 +5,27 @@
 #include "dataTypes.h"
 #include "imdbTAD.h"
 
-int readFile(char fileName[], imdbADT data);
+/*
+ * Funcion:     readFile
+ * Uso:         c = readFile(fileName, data);
+ * ----------------------------------------------------------------------
+ * Descripcion: Lee los datos del .csv y la almacena en el imdbTAD.
+ * ----------------------------------------------------------------------
+ * Recibe:      Puntero a estructura de datos, y nombre del archivo a leer
+ * Devuelve:    Devuelve flag prendido si tuvo error al abrir el file
+ */
+int readFile(imdbADT data, char * fileName);
 
-void processData(imdbADT data);
+/*
+ * Funcion:     writeData
+ * Uso:         c = writeData(fileName, data);
+ * ----------------------------------------------------------------------
+ * Descripcion: Escribe los datos de la estructura en los archivos
+ *              correspondientes a los queries
+ * ----------------------------------------------------------------------
+ * Recibe:      Puntero a estructura de datos
+ * Devuelve:    VOID
+ */
+void writeData(imdbADT data);
 
 #endif //TPE_FINAL_FRONTEND_H
